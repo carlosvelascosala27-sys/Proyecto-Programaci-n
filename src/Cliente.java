26677e664195ea331628fc99d2c7366324bd9ae5
Carlos Velasco <carlos.velascosala27@gmail.com>
2026-03-06 18:32:00 +0100

    Terminado las clase Cliente a falta de retoques

--- src/Cliente.java
+++ src/Cliente.java
@@ -9,5 +9,43 @@ public class Cliente {
     private String direccion;
     private ArrayList<Pedido> historial;
 
-    
+    public Cliente(String nombre,  String apellidos, String telefono, String direccion) {
+        this.nombre = nombre.toLowerCase();
+        this.apellidos = apellidos.toUpperCase();
+        this.telefono = telefono;
+        this.direccion = direccion;
+        this.fechaAlta = new Date();
+        this.historial = new ArrayList<>();
+    }
+
+    public boolean agregarPedido(Pedido pedido) {
+        if(pedido == null) {
+            return false;
+        }
+
+        if(pedido.getEstado() != EstadoPedido.PAGADO){
+            return false;
+        }
+
+        if (pedido.getCliente() != this) {
+            return false;
+        }
+
+        for (int i = 0; i < historial.size(); i++) {
+            Pedido p = historial.get(i);
+
+            if (p.getFechaHora().equals(pedido.getFechaHora())) {
+                return false;
+            }
+        }
+
+        historial.add(pedido);
+        return true;
+    }
+
+
+    public Pedido realizarPedido() {
+        Pedido pedido = new Pedido(this);
+        return pedido;
+    }
 }
