c7a0ac2bd9ca9706482226aa3ff30b4cf0e58368
Carlos Velasco <carlos.velascosala27@gmail.com>
2026-03-06 18:40:22 +0100

    Se necesita el get en la clase Cliente

--- src/GestionPedidos.java
+++ src/GestionPedidos.java
@@ -1,2 +1,33 @@
+import java.util.ArrayList;
+
 public class GestionPedidos {
+    private ArrayList<Cliente> clientes;
+    private ArrayList<Producto> productos;
+
+
+    public GestionPedidos() {
+        clientes = new ArrayList<>();
+        productos = new ArrayList<>();
+    }
+
+    public void agregarCliente(Cliente cliente) {
+        clientes.add(cliente);
+    }
+
+    public void agregarProducto(Producto producto) {
+        productos.add(producto);
+    }
+
+    public Cliente buscarCliente(String telefono) {
+
+        for (int i = 0; i < clientes.size(); i++) {
+            Cliente c = clientes.get(i);
+
+            if (c.getTelefono().equals(telefono)) {
+                return c;
+            }
+        }
+
+        return null;
+    }
 }
