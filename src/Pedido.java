c131cd4ecddcad481997a8564f1739d9efd69d75
Carlos Velasco <carlos.velascosala27@gmail.com>
2026-03-08 16:10:16 +0100

    Realizado todos los comentarios necesarios y javadoc

--- src/Pedido.java
+++ src/Pedido.java
@@ -31,6 +31,10 @@ public class Pedido {
         return importeTotal;
     }
 
+    public PasarelaDePago getPago(){
+        return pago;
+    }
+
     public boolean agregarProducto(Producto producto) {
         if (estado == EstadoPedido.PAGADO) {
             return false;
@@ -104,8 +108,10 @@ public class Pedido {
     public String toString() {
         String contenido = "";
 
-        contenido += "CANT.\tPRODUCTO\tPRECIO UD.\tTOTAL\n";
-        contenido += "==============================================\n";
+        //Alinear los datos según como nos piden, reclcar que desconocía este forma de colocar los números,
+        // gracias a videos y la ia me han ayudado a colocar mejor los números
+        contenido += String.format("%-5s %-12s %-12s %-8s\n", "CANT.", "PRODUCTO", "PRECIO UD.", "TOTAL");
+        contenido += "===============================================\n";
 
 
         ArrayList<String> lista = new ArrayList<>();
@@ -124,13 +130,15 @@ public class Pedido {
 
                 totalProductos = Math.round(totalProductos * 100.0) / 100.0;
 
-                contenido += cantidad + "\t" + nombre + "\t" + precioUnidad + "\t" + totalProductos + "\n";
+                contenido += String.format("%-5d %-12s %-12.2f %-8.2f\n", cantidad, nombre, precioUnidad, totalProductos);
+
                 lista.add(nombre);
             }
         }
 
         contenido += "==============================================\n";
-        contenido += "TOTAL: " + this.importeTotal + " €\n";
+        contenido += "TOTAL --------------------------------------------> "
+                + String.format("%.2f", this.importeTotal) + " €\n";
 
         return contenido;
 
